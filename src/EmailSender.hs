f274cb1ff8ec7e0f364402e6a7b6fd33b2677b56
Rohit <RohitSinghEmail@protonmail.com>
2024-08-18 17:11:40 +0530

    Email schema change

--- src/EmailSender.hs
+++ src/EmailSender.hs
@@ -7,19 +7,24 @@ module EmailSender
   , formatAttachment
   ) where
 
-import           Configuration.Dotenv        (loadFile, defaultConfig)
+-- import           Configuration.Dotenv        (defaultConfig, loadFile)
+import qualified Data.ByteString.Lazy        as BL
+import           Data.Maybe
 import           Data.MIME.Types             (defaultmtd, guessType)
 import qualified Data.Text                   as T
-import qualified Data.Text.Lazy              as LT
 import qualified Data.Text.IO                as TIO
+import qualified Data.Text.Lazy              as LT
 import           Network.HaskellNet.Auth     (AuthType (LOGIN))
 import           Network.HaskellNet.SMTP     (authenticate, closeSMTP, sendMail)
 import           Network.HaskellNet.SMTP.SSL (connectSMTPSSL)
-import           Network.Mail.Mime           (addAttachments, plainPart, Address(..), Mail(..))
-import           System.IO                   (openFile, hSetBuffering, hClose, IOMode (ReadMode), BufferMode (BlockBuffering))
+import           Network.Mail.Mime           (Address (..), Mail (..),
+                                              addAttachments, addAttachmentsBS,
+                                              htmlPart, plainPart)
 import           System.Environment          (getEnv)
 import           System.Exit                 (die)
-
+import           System.IO                   (BufferMode (BlockBuffering),
+                                              IOMode (ReadMode), hClose,
+                                              hSetBuffering, openFile)
 import           Types
 
 formatAddress :: T.Text -> Address
@@ -33,18 +38,33 @@ formatAttachment attDir file = (mimeType, path)
       (Just t, _)  -> T.pack t
     path = attDir <> "/" <> T.unpack file
 
+formatAttachments' :: [Attachment] -> ([(T.Text, FilePath)], [(T.Text, T.Text, BL.ByteString)])
+formatAttachments' [] = ([], [])
+formatAttachments' ((Attachment fn fc ft):xs) = case fc of
+                             AttachmentLocalPath p -> ((fromMaybe "text/plain" ft, T.unpack p) : fst go, snd go)
+                             AttachmentRemoreUri u -> undefined
+                             AttachmentBase64 b -> undefined
+                  where go = formatAttachments' xs
+
 sendEmail :: Email -> IO ()
 sendEmail email = do
   password <- getEnv "PASS"
+
+  let bodyPart = case emailBody email of
+                EmailBody (Just textContent) (Just htmlContent) -> [plainPart $ LT.fromStrict textContent, htmlPart $ LT.fromStrict htmlContent]
+                EmailBody Nothing (Just htmlContent) -> [htmlPart $ LT.fromStrict htmlContent]
+                EmailBody (Just textContent) Nothing -> [plainPart $ LT.fromStrict textContent]
+                EmailBody Nothing Nothing -> []
   let mail = Mail
         { mailFrom = emailFrom email
         , mailTo = emailTo email
         , mailCc = emailCc email
         , mailBcc = emailBcc email
         , mailHeaders = [("Subject", emailSubject email)]
-        , mailParts = [[plainPart $ LT.fromStrict $ emailBody email]]
+        , mailParts = [bodyPart]
         }
-  mailWithAttachments <- addAttachments (emailAttachments email) mail
+  let formattedAttachments = formatAttachments' $ emailAttachments email
+  mailWithAttachments <- addAttachments (fst formattedAttachments) (addAttachmentsBS (snd formattedAttachments) mail)
   conn <- connectSMTPSSL "smtp.gmail.com"
   authSucceed <- authenticate LOGIN "rohitsingh.mait@gmail.com" password conn
   if authSucceed
@@ -53,26 +73,42 @@ sendEmail email = do
   closeSMTP conn
 
 processEmail :: String -> String -> IO ()
-processEmail emailDir email = do
-  fh <- openFile (emailDir <> "/" <> email <> ".txt") ReadMode
+processEmail emailDir emailName = do
+  fh <- openFile (emailDir <> "/" <> emailName <> ".txt") ReadMode
   hSetBuffering fh (BlockBuffering Nothing)
-  
+
   -- Read the lines from the file using Text.IO functions
   addressLine <- T.words <$> TIO.hGetLine fh
   let addresses = map formatAddress addressLine
-  attachmentsLine <- T.words <$> TIO.hGetLine fh
-  let attachments = map (formatAttachment "/home/rohits/mydata/code/git_repos/email-sender/attachments/") attachmentsLine
+  -- attachmentsLine <- T.words <$> TIO.hGetLine fh
+  -- let attachments = map (formatAttachment "/home/rohits/mydata/code/git_repos/email-sender/attachments/") attachmentsLine
   subject <- TIO.hGetLine fh
   content <- TIO.hGetContents fh
 
-  let email = Email
+  let body = EmailBody
+        { emailBodyText = Just content
+        , emailBodyHtml = Nothing
+        }
+
+  let attachments =
+        [
+          Attachment {
+              attachmentFilename = "Resume_H.pdf"
+            , attachmentContent = AttachmentLocalPath "/home/rohits/mydata/code/git_repos/email-shooter/attachments/Resume_H.pdf"
+            , attachmentContentType = Just "application/pdf"
+          }
+
+        ]
+
+
+  let emailData = Email
         { emailFrom = Address Nothing "rohitsingh.mait@gmail.com"
         , emailTo = addresses
         , emailCc = []
         , emailBcc = [Address Nothing "rohitsingh.mait@gmail.com"]
         , emailSubject = subject
-        , emailBody = content
+        , emailBody = body
         , emailAttachments = attachments
         }
-  sendEmail email
+  sendEmail emailData
   hClose fh
--- src/Types.hs
+++ src/Types.hs
@@ -1,21 +1,39 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Types
   ( Email(..)
+  , EmailBody(..)
+  , Attachment(..)
+  , AttachmentContent(..)
   , Address(..)
   , fromText
   ) where
 
-import qualified Data.Text as T
-import           Data.Text.Lazy.Builder (fromText)  -- Import fromText
-import           Network.Mail.Mime (Address(..))
+import qualified Data.Text              as T
+import           Data.Text.Lazy.Builder (fromText)
+import           Network.Mail.Mime      (Address (..))
+
+data EmailBody = EmailBody
+  { emailBodyText :: Maybe T.Text
+  , emailBodyHtml :: Maybe T.Text
+  } deriving (Show, Eq)
+
+data AttachmentContent
+  = AttachmentLocalPath T.Text
+  | AttachmentRemoreUri T.Text
+  | AttachmentBase64 T.Text
+  deriving (Show, Eq)
+
+data Attachment = Attachment
+  { attachmentFilename    :: T.Text
+  , attachmentContent     :: AttachmentContent
+  , attachmentContentType :: Maybe T.Text
+  } deriving (Show, Eq)
 
 data Email = Email
-  { emailFrom    :: Address
-  , emailTo      :: [Address]
-  , emailCc      :: [Address]
-  , emailBcc     :: [Address]
-  , emailSubject :: T.Text
-  , emailBody    :: T.Text
-  , emailAttachments :: [(T.Text, FilePath)]
-  }
+  { emailFrom        :: Address
+  , emailTo          :: [Address]
+  , emailCc          :: [Address]
+  , emailBcc         :: [Address]
+  , emailSubject     :: T.Text
+  , emailBody        ::  EmailBody
+  , emailAttachments :: [Attachment]
+  } deriving (Show, Eq)
